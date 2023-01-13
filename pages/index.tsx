7fa373202bd8d4e9b117abfedb219334ca50fba9
maitriitaliya211 <maitriitaliya211@gmail.com>
2023-01-13 15:33:44 +0530

    change

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,7 @@ const Home = ({
   return (
     <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
       <Head>
-        <title>Home - Netflix</title>
+        <title>Home -jaydeep</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
